165b1dd113ab0c01df1555e17796619e213b04d4
krhistopher <krhistopher@weon.com.br>
2023-01-31 16:55:09 -0300

    css aplicado

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -3,34 +3,36 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Kanit&display=swap" rel="stylesheet">
-        <table id="bordMagic">
-            <tr id="NomenclaturaEditaveis"> 
-                <td> ID </td>
-                <td> Nome </td>
-                <td> Idade </td>
-                <td> Cep </td>
-                <td> Cidade </td>
-                <td> Estado </td>
-                <td> Editar Item</td>
-                <td> Deletar Item</td>
-            </tr>
-            
-            <tr id="itemsEditaveis" v-for="items in myListArray" :key="items.key">
-                <td> {{ items.id }}</td>
-                <td> {{ items.nome }}</td>
-                <td> {{ items.idade }}</td>
-                <td> {{ items.Local.cep }}</td>
-                <td> {{ items.Local.cidade }}</td>
-                <td> {{ items.Local.estado }}</td>
-                <td> <button type="button" @click="editObject(items.id)"> Edit Object</button> </td>
-                <td> <button type="button" @click="deletAll(items.id)"> Delet Object</button> </td>
-            </tr>
-            <hr>
-            <div id="foraDoEscopo">
-            <button type="button" @click="addNewItem"> Add New Item</button>
-            <button type="button" @click="DoubleAge"> Multiplier Age</button>
-            </div>
-        </table>
+        <h1> Listar Usuários </h1>
+        <div id="ControlTable">
+            <table id="ControlIntegerTable">
+                <tr id="TableHeader"> 
+                    <td> ID </td>
+                    <td> Nome </td>
+                    <td> Idade </td>
+                    <td> Cep </td>
+                    <td> Cidade </td>
+                    <td> Estado </td>
+                    <td> Editar Item</td>
+                    <td> Deletar Item</td>
+                </tr>
+                
+                <tr id="itemsTable" v-for="items in myListArray" :key="items.key">
+                    <td> {{ items.id }}</td>
+                    <td> {{ items.nome }}</td>
+                    <td> {{ items.idade }}</td>
+                    <td> {{ items.Local.cep }}</td>
+                    <td> {{ items.Local.cidade }}</td>
+                    <td> {{ items.Local.estado }}</td>
+                    <td> <button type="button" @click="editObject(items.id)"> Edit Object</button> </td>
+                    <td> <button type="button" @click="deletAll(items.id)"> Delet Object</button> </td>
+                </tr>
+                <div id="GlobalUpdateButtons">
+                    <button type="button" @click="addNewItem"> Add New Item</button>
+                    <button type="button" @click="DoubleAge"> Multiplier Age</button>
+                </div>
+            </table>
+        </div>
 
     </div>
     
@@ -139,57 +141,105 @@ export default {
 
 <style scoped>
 
-#bordMagic{
-    border: 3px solid black;
-    padding: 0px 0px 0px 0px;
+#homePage {
+    align-items: center;
     font-family: 'Kanit', sans-serif;
 
 }
 
-#NomenclaturaEditaveis{
-    background-color: rgba(0, 0, 0, 0.63);
-    color: white;
+h1 {
+    text-align: center;
 }
 
-#itemsEditaveis {
-    background-color: rgba(0, 0, 0, 0.863);
-    color: white;
+td{ 
+    padding: 20px 50px 20px 50px;
+   
 }
 
-#bordMagic td button {
-    background-color: rgba(255, 255, 255, 0.397);
-    border: 2px solid white;
-    padding: 10px;
-    color: white;
-}
-
-#bordMagic td button:hover{
-    border: 2px solid rgb(255, 0, 0);
-    background-color: red;
+#ControlTable {
+    display: flex;
+    justify-content: center;
+    background-color: rgba(5, 0, 0, 0.192);
+    margin-right: 100px;
+    margin-left: 100px;
+    
 
 }
 
-#bordMagic td {
-    border: 0px solid black;
-    padding: 5px 40px 30px 5px;
+#ControlIntegerTable {
+    border-collapse: collapse;
+    background-color: white;
+
 }
 
-hr {
+
+#TableHeader {
+   background-color: rgb(82, 82, 240); 
+   font-size: large;
+   
+}
+
+#itemsTable {
+    background-color: white;
     border: 1px solid black;
+    
 }
 
-#foraDoEscopo button {
-    background-color: rgba(0, 0, 0, 0.205);
-    border: 2px solid rgb(0, 0, 0);
-    padding: 20px;
-    color: rgb(0, 0, 0);
-    margin: 1px 3px 1px 3px
+#itemsTable button {
+    margin: 5px 7px;
+    padding: 10px;
+    font-size: medium;
+    background-color: rgb(3, 187, 3);
+    box-shadow:0 5px 0 #006000;
+    border: 1px solid
+    
 }
 
-#foraDoEscopo button:hover {
-    border: 2px solid rgb(0, 0, 0);
-    background-color: red;
-
+#itemsTable button:hover {
+    background:#006000;
+    box-shadow:0 5px 0 #003f00;
+    
 }
+
+#itemsTable button:active {
+    background-color: rgb(255, 230, 0);
+    position: relative;
+    top: 5px;
+    box-shadow: none;
+    
+}
+
+#GlobalUpdateButtons {
+    display: flex;
+    justify-content: center;
+}
+
+#GlobalUpdateButtons button {
+    font-size: medium;
+    border: 1px solid;
+    margin-top: 2px;
+    margin-left: 1px;
+    background-color: rgb(3, 187, 3);
+    box-shadow:0 5px 0 #006000;
+    border: 1px solid;
+    padding: 0px 25px 0px 25px;
+}
+
+#GlobalUpdateButtons button:hover {
+    background:#006000;
+    box-shadow:0 5px 0 #003f00;
+    
+}
+
+#GlobalUpdateButtons button:active {
+    background-color: rgb(255, 230, 0);
+    position: relative;
+    top: 5px;
+    box-shadow: none;
+    
+}
+
 
 </style>
+
+topo direita inferior esquerda
